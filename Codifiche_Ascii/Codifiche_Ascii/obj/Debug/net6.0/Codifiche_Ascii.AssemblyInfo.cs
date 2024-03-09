e58c1b53a98dc5ab32120e568b256ee0f8191802
Giulia Montalti <GIULIA.MONTALTI@local.ittcesena.it>
2024-03-09 11:31:11 +0100

    Primo Upload

--- Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
+++ Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee619a820a890050a5fafff0a069976bc494cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
